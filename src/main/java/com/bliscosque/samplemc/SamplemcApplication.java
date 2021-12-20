c98e3338257b7339580ab4ed7e5e8b613cd500d3
Thiago Bliscosque Goncalves <bliscosque@yahoo.com.br>
2021-12-20 09:29:56 -0600

    Projeto criado

---
+++ src/main/java/com/bliscosque/samplemc/SamplemcApplication.java
@@ -0,0 +1,13 @@
+package com.bliscosque.samplemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplemcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplemcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bliscosque/samplemc/SamplemcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bliscosque.samplemc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplemcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
