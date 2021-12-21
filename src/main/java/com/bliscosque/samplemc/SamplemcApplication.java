4cf9453bf399ad383f43f5f69a3f1eefd8e0879d
Thiago Bliscosque Goncalves <bliscosque@yahoo.com.br>
2021-12-20 21:59:24 -0600

    Instanciacao

--- src/main/java/com/bliscosque/samplemc/SamplemcApplication.java
+++ src/main/java/com/bliscosque/samplemc/SamplemcApplication.java
@@ -1,13 +1,32 @@
 package com.bliscosque.samplemc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bliscosque.samplemc.domain.Categoria;
+import com.bliscosque.samplemc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SamplemcApplication {
+public class SamplemcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SamplemcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
